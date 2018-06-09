7c0948bb2657365f8e19f5d5a069daf560040b82
SaumilBapat <sbapat@salesforce.com>
2018-06-09 03:14:13 -0400

    Updated Server

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 console.log("Starting Server");
-/*
 const express = require('express');
+
 var app = express();
 
-app.get('/start', (req, res) => {
+app.get('/', function(req, res) {
     return res
         .status(200)
         .send('App First Page');
 });
-*/
