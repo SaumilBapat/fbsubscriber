c59175765446bdbe0f54b0441c87b63cede2f10e
SaumilBapat <sbapat@salesforce.com>
2018-06-09 03:23:21 -0400

    Updated Server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
 // respond with "hello world" when a GET request is made to the homepage
+app.listen(3000, () => {
+    console.log("Started on port 3000");
+});
+
 app.get('/', function (req, res) {
   res.send('hello world')
-})
+});
